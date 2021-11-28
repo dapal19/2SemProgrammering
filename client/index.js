6ba974e8c1e323802b25947a19f1f1a5b05650e9
Mengsie <Mikkelm007@outlook.dk>
2021-11-28 15:40:03 +0100

    html og post

--- client/index.js
+++ client/index.js
@@ -6,4 +6,33 @@ document.addEventListener("DOMContentLoaded", function(){
 
         console.log('hej')
     })
-})
+
+    let submit = document.getElementById("submit")
+
+    submit.addEventListener("click", (e) =>{
+        e.preventDefault();
+
+        let username = document.getElementById("username").value;
+
+        var uniq = 'id' + Date.now().toString(36)
+        let newUser = {
+      id: uniq,
+      user: username
+     }
+        console.log(newUser)
+
+        fetch("/user",{
+            method: "POST",
+            headers: {
+                'content-Type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        }) .then(response => response.json())
+        .then(data => {
+        console.log(data)
+        })
+        .catch((error) => {
+        console.log('error:', error)
+        })
+    })
+});
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const app = express();
 const port = 4000;
+const fs = require("fs")
 
-app.get('/', (req, res) =>{
-    res.send('hello world')
 
-})
+app.use(express.json())
 
+app.use(express.static('client'))
+
+app.get('/', (req,res) => {
+   res.sendFile(path.join(__dirname, "/client/index.html"));
+}); 
+
+
+
+app.post('/user', (req,res) => {
+
+    console.log(req.body)
+
+    res.send({ msg: "hello"})
+ }); 
+ 
 
 // Start Server
 app.listen(port, () => {
